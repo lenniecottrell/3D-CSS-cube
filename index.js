f7ddf86914a17b89bcecc24bf319ffbe7cb7da81
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-02-18 16:57:16 -0800

    change page title

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let cube = document.querySelector(".cube");
 let radioGroup = document.querySelector(".radio-group");
 let body = document.querySelector("body");
 let currentClass = "";
+let colors = ["#159c03", "#8c76b0", "#d93434", "#d97934", "#cc7014", "#95cc14"];
 
 const changeSide = () => {
   let radioSelect = radioGroup.querySelector(":checked");
@@ -12,6 +13,31 @@ const changeSide = () => {
   }
   cube.classList.add(showClass);
   currentClass = showClass;
+
+  //I'm not sure if this is necessary... seems a but much
+
+  // switch (radioSelect.value) {
+  //   case "front":
+  //     body.style.background = colors[0];
+  //     break;
+  //   case "back":
+  //     body.style.background = colors[1];
+  //     break;
+  //   case "right":
+  //     body.style.background = colors[2];
+  //     break;
+  //   case "left":
+  //     body.style.background = colors[3];
+  //     break;
+  //   case "top":
+  //     body.style.background = colors[4];
+  //     break;
+  //   case "bottom":
+  //     body.style.background = colors[5];
+  //     break;
+  //   default:
+  //     body.style.background = colors[5];
+  // }
 };
 
 changeSide();
